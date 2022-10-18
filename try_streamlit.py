85e7b83e40f92260466037bc20157cabd50aad56
Ye67890 <81035275+Ye67890@users.noreply.github.com>
2022-10-18 19:44:52 +0800

    Update try_streamlit.py

--- try_streamlit.py
+++ try_streamlit.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 
 @st.cache
-path = 'https://github.com/Ye67890/try/blob/main/%E7%A6%8F-%E7%B6%93%E7%B7%AF%E5%BA%A6.csv'
-data = pd.read_csv(path)
+def load_data():
+         data = pd.read_csv('福-經緯度.csv')
+         return data
 
 st.write("""
 # 第一個應用程式
